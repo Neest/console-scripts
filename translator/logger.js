292e5e6bb62170f6b97a37796edb21629b2557fe
Neest <denyska7@yandex.ru>
2016-06-10 12:50:18 +0300

    Logger will generate common log file with all words ever translated

--- translator/logger.js
+++ translator/logger.js
@@ -7,17 +7,18 @@ class Logger {
   log(raw, translated) {
     let couple, filename;
 
-    couple = `${raw} -- ${translated}`;
+    couple = `${raw} -- ${translated}\n`;
     filename = this._generateFilename();
 
     fs.appendFile(filename, couple, (err) => console.log(err || translated) );
+    fs.appendFile(path.join(__dirname, 'logs/all.log'), couple);
   }
 
   _generateFilename() {
     let date, monthNames;
 
     date = new Date();
-    monthNames = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    monthNames = 'Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec'.split(/\s+/);
 
     return path.join(__dirname, `logs/${date.getDate()} ${monthNames[date.getMonth()]}, ${date.getFullYear()}.log`);
   }
